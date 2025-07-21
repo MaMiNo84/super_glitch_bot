21ccdb4955486c1d0d4fe074a3b26439db559ef0
MaMiNo84 <164735747+MaMiNo84@users.noreply.github.com>
2025-07-21 23:12:17 +0200

    Enhance chart generator with Moralis data

--- tests/test_chart_generator.py
+++ tests/test_chart_generator.py
@@ -4,12 +4,16 @@ import os
 
 sys.path.insert(0, str(pathlib.Path(__file__).resolve().parents[1]))
 
+from datetime import datetime, timedelta
+
 from super_glitch_bot.services.chart_generator import ChartGenerator
 
 
 def test_generate_creates_image_and_cleans_up():
     gen = ChartGenerator()
-    path = gen.generate("token", [1, 2, 3, 4])
+    now = datetime.utcnow()
+    data = [(now + timedelta(minutes=i), float(i)) for i in range(4)]
+    path = gen.generate("token", data)
     assert os.path.exists(path)
     gen.cleanup()
     assert not os.path.exists(path)
